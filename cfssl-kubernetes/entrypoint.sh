7a7eabda488263051d8dadc729ef4507d4af25d9
Brandon B. Jozsa <bjozsa@jinkit.com>
2018-04-09 15:52:40 -0600

    cfssl: refinement and more grainular control of CN=user

--- cfssl-kubernetes/entrypoint.sh
+++ cfssl-kubernetes/entrypoint.sh
@@ -6,14 +6,14 @@ source /usr/local/share/cfssl/.vars
 # Generate json files with user-defined variables:
 (envsubst < ${CFSSL_INPUT}/ca-config.json.in) > ${CFSSL_OUTPUT}/ca-config.json
 (envsubst < ${CFSSL_INPUT}/ca-csr.json.in) > ${CFSSL_OUTPUT}/ca-csr.json
-(envsubst < ${CFSSL_INPUT}/kubernetes-csr.json.in) > ${CFSSL_OUTPUT}/kubernetes-csr.json
+(envsubst < ${CFSSL_INPUT}/kubernetes-csr.json.in) > ${CFSSL_OUTPUT}/${CFSSL_CN}-csr.json
 
 # Generate cert artifacts:
 cd ${CFSSL_OUTPUT}
 /go/bin/cfssl gencert -initca ca-csr.json | /go/bin/cfssljson -bare ca
 /go/bin/cfssl gencert -ca=ca.pem -ca-key=ca-key.pem \
     --config=ca-config.json -profile=kubernetes \
-    kubernetes-csr.json | /go/bin/cfssljson -bare server
+    ${CFSSL_CN}-csr.json | /go/bin/cfssljson -bare ${CFSSL_CN} 
 
 # Now move generated cert artifacts to the pki folder:
 mv *.pem ${CFSSL_PKI}/
