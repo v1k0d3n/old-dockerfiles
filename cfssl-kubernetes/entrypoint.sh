eb5a3fe5b7778d10e088a9b585d703784b273f2c
Brandon B. Jozsa <bjozsa@jinkit.com>
2018-04-09 15:42:00 -0600

    cfssl: refactor for build args

--- cfssl-kubernetes/entrypoint.sh
+++ cfssl-kubernetes/entrypoint.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 # Source user-defined variables:
-source /example/example.vars
+#source /example/example.vars
+source /usr/local/share/cfssl/.vars
 
 # Generate json files with user-defined variables:
-(envsubst < /example/ca-config.json.in) > /example/ca-config.json
-(envsubst < /example/ca-csr.json.in) > /example/ca-csr.json
-(envsubst < /example/kubernetes-csr.json.in) > /example/kubernetes-csr.json
+(envsubst < ${CFSSL_INPUT}/ca-config.json.in) > ${CFSSL_OUTPUT}/ca-config.json
+(envsubst < ${CFSSL_INPUT}/ca-csr.json.in) > ${CFSSL_OUTPUT}/ca-csr.json
+(envsubst < ${CFSSL_INPUT}/kubernetes-csr.json.in) > ${CFSSL_OUTPUT}/kubernetes-csr.json
 
 # Generate cert artifacts:
-cd /example
+cd ${CFSSL_OUTPUT}
 /go/bin/cfssl gencert -initca ca-csr.json | /go/bin/cfssljson -bare ca
 /go/bin/cfssl gencert -ca=ca.pem -ca-key=ca-key.pem \
     --config=ca-config.json -profile=kubernetes \
-    kubernetes-csr.json | /go/bin//cfssljson -bare server
+    kubernetes-csr.json | /go/bin/cfssljson -bare server
 
 # Now move generated cert artifacts to the pki folder:
-mv *.pem /pki/
-mv *.csr /pki/
+mv *.pem ${CFSSL_PKI}/
+mv *.csr ${CFSSL_PKI}/
+mkdir ${CFSSL_PKI}/input/
+cp -R ${CFSSL_OUTPUT}/* ${CFSSL_PKI}/input/
